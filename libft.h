ed38dbefbb88be40e76a311919226895a2685bd0
Jonne Ervasti <jervasti@c2r1p4.hive.fi>
2022-10-26 18:40:23 +0300

    tofunctions

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jervasti <jervasti@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 14:53:51 by jervasti          #+#    #+#             */
-/*   Updated: 2022/10/25 21:51:36 by jervasti         ###   ########.fr       */
+/*   Updated: 2022/10/26 18:38:05 by jervasti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,21 @@
 # include <stdlib.h>
 
 //Part 1 - libc functions
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-int	ft_isprint(int c);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
 size_t	ft_strlen(const char *s);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
 
 //Part 2 - Additional functions
 
 //Part 3 - Bonus functions
 
 //Part 4 - Own functions
-int	ft_isupper(int c);
-int	ft_islower(int c);
+int		ft_isupper(int c);
+int		ft_islower(int c);
 
 #endif
